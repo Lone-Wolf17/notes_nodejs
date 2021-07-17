8a69cab71cd3d7ea93eb47888701e812f837f7e4
lonewolf <adeyanju.akorede1@gmail.com>
2021-07-17 06:35:35 +0100

    converted generated code to Es6 syntax

--- app.js
+++ app.js
@@ -1,41 +1,91 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import { default as express } from 'express';
+import { default as hbs } from 'hbs';
+import * as path from 'path';
+// import * as favicon from 'serve-favicon';
+import { default as logger } from 'morgan';
+import { default as cookieParser } from 'cookie-parser';
+import { default as bodyParser } from 'body-parser';
+import * as http from 'http';
+import { approotdir } from './approotdir.js';
+import { 
+  normalizePort, onError, onListening, handle404, basicErrorHandler
+} from './appsupport.js';
+import { router as indexRouter } from './routes/index.js';
+// import { router as notesRiuter } from './routes/notes.js';
+const __dirname = approotdir;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+export const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'partails'));
 
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Router function lists 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/notes', notesRouter);
 
+// error handlers 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(handle404);
+app.use(basicErrorHandler);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+export const port = normalizePort(process.env.PORT || 3000);
+app.set('port', port);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+export const server = http.createServer(app);
 
-module.exports = app;
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
+import { default as express } from 'express';
+export const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  //.. Placeholder for Notes home page code
+  res.render('index', {title: 'Notes'});
 });
-
-module.exports = router;
