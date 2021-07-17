255a8d3589901d6258519e5c6b7e7aef3b8b7765
lonewolf <adeyanju.akorede1@gmail.com>
2021-07-17 12:01:33 +0100

    Implement Note Model & In Memory Datastore

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ import {
   normalizePort, onError, onListening, handle404, basicErrorHandler
 } from './appsupport.js';
 import { router as indexRouter } from './routes/index.js';
-// import { router as notesRiuter } from './routes/notes.js';
+import { router as notesRouter } from './routes/notes.js';
+
+import { InMemoryNotesStore } from './models/notes-memory.js';
+export const NotesStore = new InMemoryNotesStore();
+
 const __dirname = approotdir;
 
 export const app = express();
@@ -19,7 +23,7 @@ export const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-hbs.registerPartials(path.join(__dirname, 'partails'));
+hbs.registerPartials(path.join(__dirname, 'partials'));
 
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -30,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Router function lists 
 app.use('/', indexRouter);
-// app.use('/notes', notesRouter);
+app.use('/notes', notesRouter);
 
 // error handlers 
 // catch 404 and forward to error handler
@@ -45,47 +49,3 @@ export const server = http.createServer(app);
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
-
-
-
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-// var app = express();
-
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hbs');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
---
+++ routes/notes.js
@@ -0,0 +1,7 @@
+// const util = require ('util');
+
+import { default as express } from 'express';
+import { NotesStore as notes } from '../app.js';
+
+export const router = express.Router();
+
